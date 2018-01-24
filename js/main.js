b7799373ff2ade463e291d1eb60f8b56061b0f0b
momo7164 <smmo73@gmail.com>
2018-01-24 18:29:50 +0900

    20180124 수정

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(function() {
 	//map.mapTypes.set(naver.maps.MapTypeId.NORMAL, naver.maps.NaverMapTypeOption.getNormalMap());
 	//map.mapTypes.set(naver.maps.MapTypeId.HYBRID, naver.maps.NaverMapTypeOption.getHybridMap());
 	
-	var cadastralLayer = new naver.maps.CadastralLayer();			
+	var cadastralLayer = new naver.maps.CadastralLayer();		
 
 	function showJijeokLayer(onOff, $btn) {		  
 		if(onOff == 'on') {
@@ -30,6 +30,8 @@ $(function() {
 			cadastralLayer.setMap(map);
 		}
 	}
+
+	
 
 
 	//지적도
@@ -61,20 +63,22 @@ $(function() {
 	//일반보기
 	$('#btnMapNormal').on('click', function()  {
 		map.setMapTypeId(naver.maps.MapTypeId.NORMAL);
+		$(this).addClass('active');
+		$('#btnMapSatellite').removeClass('active');
 	});
 
 	//위성보기
 	$('#btnMapSatellite').on('click', function()  {
 		map.setMapTypeId(naver.maps.MapTypeId.HYBRID);
+		$(this).addClass('active');
+		$('#btnMapNormal').removeClass('active');
 	});
-
-
-	$('#dvIntro').hide();
-	$('#dvTimeview').hide();
+	
 });
 
+
 // 우측 버튼 active 관련
-function showRightBtn(onOff, $btn) {		  
+var showRightBtn = function(onOff, $btn) {		  
 	if(onOff == 'on') {
 		$btn.data('switch', 'off');
 		$btn.removeClass('active');
@@ -86,7 +90,7 @@ function showRightBtn(onOff, $btn) {
 }
 
 // Timeview show hide
-function showTimeviewLayer(onOff, $btn) {		  
+var showTimeviewLayer = function(onOff, $btn) {		  
 	if(onOff == 'on') {
 		$('#dvTimeview').hide();
 	}
@@ -96,7 +100,150 @@ function showTimeviewLayer(onOff, $btn) {
 	}
 }
 
-//좌 검색영역 height 계산
 
+////////////////////////////////////////////////////////////////////////
+// 좌메뉴 클릭시 해당 컨텐츠 노출
+var showLnbCont = function(element) {
+	this.$element  = $(element);
+	this.$parent   = this.$element.parent('li');		
+	var data       = this.$element.data('name');
+
+	this.$parent.addClass('active');
+	this.$parent.siblings('li').removeClass('active');
+
+	$('#lnbCont > .lnbContWrap').hide();
+	$('#lnbCont > #' + data).show();
+}
+
+// 노출된 좌메뉴 컨텐츠 close 
+var hideLnbCont = function(obj){
+	var divObj = null;
+	var parents = $(obj).parents();
+	
+	for (var i=0;i<parents.length;i++) {
+		var parent = parents[i];
+		if ($(parent).hasClass('lnbContWrap')) {
+			divObj = parent;
+			break;
+		}
+	}
+
+	var thisPopId = divObj.id; 
+	$('#' + thisPopId).hide();
+	$('#memuList > li').removeClass('active');
+
+	
+	var lnbWidth   = $('#lnbArea').outerWidth();
+	$('.mapArea').animate({
+		'left': lnbWidth 
+	},100);
+}
+
+// 좌메뉴 컨텐츠 출력에 따른 mapArea 조정
+var mapAreaPosition = function(element){
+	this.$element  = $(element);
+	this.$parent   = this.$element.parent('li');		
+	var data       = this.$element.data('name');
+    var contWidth  = $('#lnbCont > #' + data).outerWidth();
+	var lnbWidth   = $('#lnbArea').outerWidth();
+	
+	var totalWidth = lnbWidth + contWidth
+	
+	//alert(totalWidth);
+	$('.mapArea').animate({
+		'left': totalWidth - 1
+	},100);
+
+	//map.setSize(getMapSize());
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// modal 레이어팝업 띄우기
+var commonPopup = function(url, param, width){
+	var eWidth = width; //레이어 팝업의  width 강제설정
+	
+	if(width == null){
+		eWidth = '';
+	}else if (width == 'full'){
+		eWidth = '96%';
+	}else{
+		eWidth = width;
+	}	
+	
+	$.ajax({
+		type : "get",
+		url : url,
+		data : param,
+		async: true,
+		success : function(data) {
+			$("#modalPopup").html(data);
+		},		
+		complete : function(){
+			$('#modalPopup').modal().find('.modal-dialog').css({
+				'width': eWidth
+			});
+		}
+	});	
+};
+
+// modal위에서 modal 레이어팝업 띄울때
+var commonPopup2 = function(url, param, width){
+	var eWidth = width; //레이어 팝업의  width 강제설정
+	
+	if(width == null){
+		eWidth = '';
+	}else if (width == 'full'){
+		eWidth = '96%';
+	}else{
+		eWidth = width;
+	}	
+	
+	$.ajax({
+		type : "get",
+		url : url,
+		data : param,
+		async: true,
+		success : function(data) {
+			$("#modalPopup2").html(data);
+		},		
+		complete : function(){
+			$('#modalPopup2').modal().find('.modal-dialog').css({
+				'width': eWidth
+			});
+		}
+	});	
+};
+
+// modal layerpopup window기준 max-height 재설정 // modal-body에 스크롤 생성
+var setModalMaxHeight = function(element) {
+	this.$element     = $(element);  
+	this.$content     = this.$element.find('.modal-content');
+	var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
+    var dialogMargin  = $(window).width() < 768 ? 20 : 60;
+    var contentHeight = $(window).height() - (dialogMargin + borderWidth);
+    var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
+    var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
+    var maxHeight     = contentHeight - (headerHeight + footerHeight);
+
+	this.$content.css({
+		'overflow': 'hidden'
+	});
+  
+	this.$element
+		.find('.modal-body').css({
+			'max-height': maxHeight,
+			'overflow-y': 'auto'
+	});
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// window resize시 액션
+$(window).resize(function() {
+	if ($('.modal.in').length != 0) {
+		setModalMaxHeight($('.modal.in'));
+	}
+});
 
 
